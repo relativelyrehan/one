06b04c32766d4dc654b753d8690b9e554ee412fa
rreel <48210765+relativelyrehan@users.noreply.github.com>
2023-05-23 21:09:59 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,14 +79,14 @@ export default function Home() {
               <QRCode
                 id="QRCode"
                 size={250}
-                value={`https://one.relativelyrehan.co/api/qr?I'd=${qr}`}
+                value={`https://one.relativelyrehan.co/api/qr?id=${qr}`}
                 level="L"
               />
               <div className="flex flex-col sm:flex-row gap-3 text-sm mt-5">
                 <button
                   onClick={() => {
                     navigator.clipboard.writeText(
-                      `https://one.relativelyrehan.co/api/qr?I'd=${qr}`
+                      `https://one.relativelyrehan.co/api/qr?id=${qr}`
                     );
                     toast.success("Link copied to clipboard");
                   }}
