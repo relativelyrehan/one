d59c0487c275becb8222261389f73838348bb052
relativelyrehan <rehan18alam@gmail.com>
2024-08-24 20:59:50 +0530

    xx

--- src/app/api/qr/route.tsx
+++ src/app/api/qr/route.tsx
@@ -37,5 +37,6 @@ export async function GET(req: NextRequest) {
     return NextResponse.redirect("/404");
   } catch (e) {
     console.error(e);
+    return NextResponse.json("Internal Server Error", { status: 500 });
   }
 }
