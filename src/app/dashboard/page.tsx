4792c8ae277a9f808c849313d9e17f41018ec490
relativelyrehan <rehan18alam@gmail.com>
2024-08-25 15:41:30 +0530

    xx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { FaAppStoreIos, FaGooglePlay } from "react-icons/fa";
-import { AuthProvider } from "@/components/AuthProvider";
 import { Button } from "@/components/Button";
 import Navbar from "@/components/Navbar";
 import { SecondScreen } from "@/components/SecondScreen";
@@ -136,125 +135,123 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <AuthProvider>
-      <section className="container mx-auto px-6">
-        <Navbar />
-        <div className="flex items-center">
-          <h1 className="text-4xl mt-10 font-semibold flex-1">
-            Your QRs ({qrList.length})
-          </h1>
-          <div>
-            <Button
-              padding="py-1 px-4"
-              className="mt-8 w-32"
-              category="white"
-              text="Generate QR"
-              type="submit"
-              onClick={() => setCreateQR(true)}
-            ></Button>
-          </div>
+    <section className="container mx-auto px-6">
+      <Navbar />
+      <div className="flex items-center">
+        <h1 className="text-4xl mt-10 font-semibold flex-1">
+          Your QRs ({qrList.length})
+        </h1>
+        <div>
+          <Button
+            padding="py-1 px-4"
+            className="mt-8 w-32"
+            category="white"
+            text="Generate QR"
+            type="submit"
+            onClick={() => setCreateQR(true)}
+          ></Button>
         </div>
+      </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-6">
-          {pending
-            ? new Array(12)
-                .fill(0)
-                .map((_, k) => (
-                  <div
-                    className="col-span-1 bg-zinc-800 animate-pulse h-80"
-                    key={k}
-                  ></div>
-                ))
-            : qrList
-                .sort((a, b) => {
-                  return (
-                    new Date(b.created_at).getTime() -
-                    new Date(a.created_at).getTime()
-                  );
-                })
-                ?.map((qr, k) => (
-                  <QRCard
-                    key={qr.id}
-                    path={qr.slug}
-                    appStoreURL={qr.app_store_url}
-                    playStoreURL={qr.play_store_url}
-                    createdAt={qr.created_at}
-                    clicks={qr.clicks}
-                    qrId={qr.id}
-                  />
-                ))}
-        </div>
-
-        <SecondScreen title="Create QR" show={createQR} onClose={handleClose}>
-          {newQr.id ? (
-            <div className="p-6">
-              <QRCard
-                key={newQr.id}
-                path={newQr.slug}
-                appStoreURL={newQr.app_store_url}
-                playStoreURL={newQr.play_store_url}
-                createdAt={newQr.created_at}
-                clicks={newQr.clicks}
-              />
-            </div>
-          ) : (
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                if (loading) return;
-                handleSubmitQR(
-                  e.currentTarget.appstore.value,
-                  e.currentTarget.playstore.value
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-6">
+        {pending
+          ? new Array(12)
+              .fill(0)
+              .map((_, k) => (
+                <div
+                  className="col-span-1 bg-zinc-800 animate-pulse h-80"
+                  key={k}
+                ></div>
+              ))
+          : qrList
+              .sort((a, b) => {
+                return (
+                  new Date(b.created_at).getTime() -
+                  new Date(a.created_at).getTime()
                 );
-              }}
-              className="w-full p-6"
-            >
-              <div className="flex flex-col gap-1 w-full">
-                <label htmlFor="appstore" className="text-lg">
-                  App Store
-                </label>
-                <div className="w-full flex items-center border border-zinc-800 rounded-md px-3 py-4 gap-2">
-                  <FaAppStoreIos className="text-gray-500" size={24} />
-                  <input
-                    type="text"
-                    id="appstore"
-                    name="appstore"
-                    placeholder="Enter App Store URL"
-                    className=" bg-black focus:outline-none w-full"
-                    autoCapitalize="off"
-                    autoComplete="off"
-                  />
-                </div>
+              })
+              ?.map((qr, k) => (
+                <QRCard
+                  key={qr.id}
+                  path={qr.slug}
+                  appStoreURL={qr.app_store_url}
+                  playStoreURL={qr.play_store_url}
+                  createdAt={qr.created_at}
+                  clicks={qr.clicks}
+                  qrId={qr.id}
+                />
+              ))}
+      </div>
+
+      <SecondScreen title="Create QR" show={createQR} onClose={handleClose}>
+        {newQr.id ? (
+          <div className="p-6">
+            <QRCard
+              key={newQr.id}
+              path={newQr.slug}
+              appStoreURL={newQr.app_store_url}
+              playStoreURL={newQr.play_store_url}
+              createdAt={newQr.created_at}
+              clicks={newQr.clicks}
+            />
+          </div>
+        ) : (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (loading) return;
+              handleSubmitQR(
+                e.currentTarget.appstore.value,
+                e.currentTarget.playstore.value
+              );
+            }}
+            className="w-full p-6"
+          >
+            <div className="flex flex-col gap-1 w-full">
+              <label htmlFor="appstore" className="text-lg">
+                App Store
+              </label>
+              <div className="w-full flex items-center border border-zinc-800 rounded-md px-3 py-4 gap-2">
+                <FaAppStoreIos className="text-gray-500" size={24} />
+                <input
+                  type="text"
+                  id="appstore"
+                  name="appstore"
+                  placeholder="Enter App Store URL"
+                  className=" bg-black focus:outline-none w-full"
+                  autoCapitalize="off"
+                  autoComplete="off"
+                />
               </div>
-              <div className="flex flex-col gap-1 w-full mt-3">
-                <label htmlFor="playstore" className="text-lg">
-                  Play Store
-                </label>
-                <div className="w-full flex items-center border border-zinc-800 rounded-md px-3 py-4 gap-2">
-                  <FaGooglePlay className="text-gray-500" size={24} />
-                  <input
-                    type="text"
-                    id="playstore"
-                    name="playstore"
-                    placeholder="Enter App Store URL"
-                    className=" bg-black focus:outline-none w-full"
-                    autoCapitalize="off"
-                    autoComplete="off"
-                  />
-                </div>
-                <Button
-                  padding="py-2 px-4"
-                  className="mt-8 w-auto"
-                  category="white"
-                  text="Generate QR"
-                  type="submit"
-                  loading={loading}
-                ></Button>
+            </div>
+            <div className="flex flex-col gap-1 w-full mt-3">
+              <label htmlFor="playstore" className="text-lg">
+                Play Store
+              </label>
+              <div className="w-full flex items-center border border-zinc-800 rounded-md px-3 py-4 gap-2">
+                <FaGooglePlay className="text-gray-500" size={24} />
+                <input
+                  type="text"
+                  id="playstore"
+                  name="playstore"
+                  placeholder="Enter App Store URL"
+                  className=" bg-black focus:outline-none w-full"
+                  autoCapitalize="off"
+                  autoComplete="off"
+                />
               </div>
-            </form>
-          )}
-        </SecondScreen>
-      </section>
-    </AuthProvider>
+              <Button
+                padding="py-2 px-4"
+                className="mt-8 w-auto"
+                category="white"
+                text="Generate QR"
+                type="submit"
+                loading={loading}
+              ></Button>
+            </div>
+          </form>
+        )}
+      </SecondScreen>
+    </section>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { AuthProvider } from "@/components/AuthProvider";
 import { Button } from "@/components/Button";
 import Navbar from "@/components/Navbar";
 import { AUTH_TOKEN_KEY } from "@/utils/constants";
@@ -39,49 +38,47 @@ export default function Profile() {
   }, []);
 
   return (
-    <AuthProvider>
-      <section className="container mx-auto px-6">
-        <Navbar />
-        {loading ? (
-          <div className="max-w-sm mt-10">
-            <div className="max-w-sm h-40 bg-zinc-800 animate-pulse mb-4"></div>
-            <div className="max-w-sm h-40 bg-zinc-800 animate-pulse mb-4"></div>
-            <div className="max-w-sm h-40 bg-zinc-800 animate-pulse mb-4"></div>
+    <section className="container mx-auto px-6">
+      <Navbar />
+      {loading ? (
+        <div className="max-w-sm mt-10">
+          <div className="max-w-sm h-40 bg-zinc-800 animate-pulse mb-4"></div>
+          <div className="max-w-sm h-40 bg-zinc-800 animate-pulse mb-4"></div>
+          <div className="max-w-sm h-40 bg-zinc-800 animate-pulse mb-4"></div>
+        </div>
+      ) : (
+        <div className="max-w-sm">
+          <div
+            className="flex items-center cursor-pointer mt-10"
+            onClick={() => router.push("/dashboard")}
+          >
+            <IoArrowBackOutline size={24} />
+            <span className="text-xl ml-2">Back</span>
           </div>
-        ) : (
-          <div className="max-w-sm">
-            <div
-              className="flex items-center cursor-pointer mt-10"
-              onClick={() => router.push("/dashboard")}
-            >
-              <IoArrowBackOutline size={24} />
-              <span className="text-xl ml-2">Back</span>
-            </div>
-            <h1 className="text-4xl mt-10 font-semibold">Profile</h1>
-            <div className="mt-10">
-              <h2 className="text-2xl font-semibold">Email</h2>
-              <p className="text-2xl">{profile.email}</p>
-            </div>
-            <div className="my-10 bg-zinc-600 h-[1px]" />
-            <div className="">
-              <h2 className="text-2xl font-semibold">Remaining QR quota</h2>
-              <p className="text-2xl">{profile.limit}</p>
-            </div>
-            <div className="my-10 bg-zinc-600 h-[1px]" />
-            <Button
-              padding="py-4 px-4"
-              className="mt-8 w-auto"
-              category="danger"
-              text="Logout"
-              type="submit"
-              onClick={() => {
-                localStorage.clear();
-                router.push("/");
-              }}
-            ></Button>
+          <h1 className="text-4xl mt-10 font-semibold">Profile</h1>
+          <div className="mt-10">
+            <h2 className="text-2xl font-semibold">Email</h2>
+            <p className="text-2xl">{profile.email}</p>
           </div>
-        )}
-      </section>
-    </AuthProvider>
+          <div className="my-10 bg-zinc-600 h-[1px]" />
+          <div className="">
+            <h2 className="text-2xl font-semibold">Remaining QR quota</h2>
+            <p className="text-2xl">{profile.limit}</p>
+          </div>
+          <div className="my-10 bg-zinc-600 h-[1px]" />
+          <Button
+            padding="py-4 px-4"
+            className="mt-8 w-auto"
+            category="danger"
+            text="Logout"
+            type="submit"
+            onClick={() => {
+              localStorage.clear();
+              router.push("/");
+            }}
+          ></Button>
+        </div>
+      )}
+    </section>
   );
 }
