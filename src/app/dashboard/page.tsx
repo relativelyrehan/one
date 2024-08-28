16fb938310d48b5542ffde1586053fea3c68212a
relativelyrehan <rehan18alam@gmail.com>
2024-08-29 00:27:32 +0530

    xx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { FaAppStoreIos, FaGooglePlay } from "react-icons/fa";
 import { Button } from "@/components/Button";
-import Navbar from "@/components/Navbar";
 import { SecondScreen } from "@/components/SecondScreen";
 import { useEffect, useState } from "react";
 import { QRCard } from "@/components/QRCard";
@@ -9,8 +8,9 @@ import toast from "react-hot-toast";
 import { AUTH_TOKEN_KEY } from "@/utils/constants";
 import { useAtom } from "jotai";
 import { refetchAtom } from "@/lib/atoms";
+import AppLayout from "@/components/AppLayout";
 
-export default function Dashboard() {
+export default function DashboardNew() {
   const [createQR, setCreateQR] = useState(false);
   const [editQR, setEditQR] = useState(false);
   const [pending, setPending] = useState(false);
@@ -135,126 +135,133 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <section className="container mx-auto px-4 lg:px-10 xl:px-16">
-      <Navbar />
-      <div className="flex items-center">
-        <h1 className="text-4xl mt-10 font-semibold flex-1">
-          Your QRs ({qrList.length})
-        </h1>
-        <div>
-          <Button
-            padding="py-1 px-4"
-            className="mt-8 w-32"
-            category="white"
-            text="Generate QR"
-            type="submit"
-            onClick={() => setCreateQR(true)}
-          ></Button>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-6">
-        {pending
-          ? new Array(12).fill(0).map((_, k) => (
-              <div
-                className="col-span-1 bg-zinc-800 animate-pulse h-96 p-6"
-                key={k}
-              >
-                <div className="h-40 w-full bg-zinc-500 animate-pulse"></div>
-                <div className="h-10 w-full bg-zinc-500 animate-pulse mb-2"></div>
-                <div className="h-10 w-full bg-zinc-500 animate-pulse mb-2"></div>
-                <div className="h-10 w-full bg-zinc-500 animate-pulse mb-2"></div>
-              </div>
-            ))
-          : qrList
-              .sort((a, b) => {
-                return (
-                  new Date(b.created_at).getTime() -
-                  new Date(a.created_at).getTime()
-                );
-              })
-              ?.map((qr, k) => (
-                <QRCard
-                  key={qr.id}
-                  path={qr.slug}
-                  appStoreURL={qr.app_store_url}
-                  playStoreURL={qr.play_store_url}
-                  createdAt={qr.created_at}
-                  clicks={qr.clicks}
-                  qrId={qr.id}
-                />
-              ))}
-      </div>
-
-      <SecondScreen title="Create QR" show={createQR} onClose={handleClose}>
-        {newQr.id ? (
-          <div className="p-6">
-            <QRCard
-              key={newQr.id}
-              path={newQr.slug}
-              appStoreURL={newQr.app_store_url}
-              playStoreURL={newQr.play_store_url}
-              createdAt={newQr.created_at}
-              clicks={newQr.clicks}
-            />
+    <AppLayout>
+      <section>
+        <div className="flex items-center">
+          {pending ? (
+            <div className="flex-1">
+              <div className="w-60 h-10 bg-zinc-500 animate-pulse"></div>
+            </div>
+          ) : (
+            <h1 className="text-4xl font-semibold flex-1">
+              Your QRs ({qrList.length})
+            </h1>
+          )}
+          <div>
+            <Button
+              padding="py-1 px-4"
+              className="w-32"
+              category="white"
+              text="Generate QR"
+              type="submit"
+              onClick={() => setCreateQR(true)}
+            ></Button>
           </div>
-        ) : (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (loading) return;
-              handleSubmitQR(
-                e.currentTarget.appstore.value,
-                e.currentTarget.playstore.value
-              );
-            }}
-            className="w-full p-6"
-          >
-            <div className="flex flex-col gap-1 w-full">
-              <label htmlFor="appstore" className="text-lg">
-                App Store
-              </label>
-              <div className="w-full flex items-center border border-zinc-800 rounded-md px-3 py-4 gap-2">
-                <FaAppStoreIos className="text-gray-500" size={24} />
-                <input
-                  type="text"
-                  id="appstore"
-                  name="appstore"
-                  placeholder="Enter App Store URL"
-                  className=" bg-black focus:outline-none w-full"
-                  autoCapitalize="off"
-                  autoComplete="off"
-                />
-              </div>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-6">
+          {pending
+            ? new Array(12).fill(0).map((_, k) => (
+                <div
+                  className="col-span-1 bg-zinc-800 animate-pulse h-96 p-6"
+                  key={k}
+                >
+                  <div className="h-40 w-full bg-zinc-500 animate-pulse"></div>
+                  <div className="h-10 w-full bg-zinc-500 animate-pulse mb-2"></div>
+                  <div className="h-10 w-full bg-zinc-500 animate-pulse mb-2"></div>
+                  <div className="h-10 w-full bg-zinc-500 animate-pulse mb-2"></div>
+                </div>
+              ))
+            : qrList
+                .sort((a, b) => {
+                  return (
+                    new Date(b.created_at).getTime() -
+                    new Date(a.created_at).getTime()
+                  );
+                })
+                ?.map((qr, k) => (
+                  <QRCard
+                    key={qr.id}
+                    path={qr.slug}
+                    appStoreURL={qr.app_store_url}
+                    playStoreURL={qr.play_store_url}
+                    createdAt={qr.created_at}
+                    clicks={qr.clicks}
+                    qrId={qr.id}
+                  />
+                ))}
+        </div>
+
+        <SecondScreen title="Create QR" show={createQR} onClose={handleClose}>
+          {newQr.id ? (
+            <div className="p-6">
+              <QRCard
+                key={newQr.id}
+                path={newQr.slug}
+                appStoreURL={newQr.app_store_url}
+                playStoreURL={newQr.play_store_url}
+                createdAt={newQr.created_at}
+                clicks={newQr.clicks}
+              />
             </div>
-            <div className="flex flex-col gap-1 w-full mt-3">
-              <label htmlFor="playstore" className="text-lg">
-                Play Store
-              </label>
-              <div className="w-full flex items-center border border-zinc-800 rounded-md px-3 py-4 gap-2">
-                <FaGooglePlay className="text-gray-500" size={24} />
-                <input
-                  type="text"
-                  id="playstore"
-                  name="playstore"
-                  placeholder="Enter App Store URL"
-                  className=" bg-black focus:outline-none w-full"
-                  autoCapitalize="off"
-                  autoComplete="off"
-                />
+          ) : (
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (loading) return;
+                handleSubmitQR(
+                  e.currentTarget.appstore.value,
+                  e.currentTarget.playstore.value
+                );
+              }}
+              className="w-full p-6"
+            >
+              <div className="flex flex-col gap-1 w-full">
+                <label htmlFor="appstore" className="text-lg">
+                  App Store
+                </label>
+                <div className="w-full flex items-center border border-zinc-800 rounded-md px-3 py-4 gap-2">
+                  <FaAppStoreIos className="text-gray-500" size={24} />
+                  <input
+                    type="text"
+                    id="appstore"
+                    name="appstore"
+                    placeholder="Enter App Store URL"
+                    className=" bg-black focus:outline-none w-full"
+                    autoCapitalize="off"
+                    autoComplete="off"
+                  />
+                </div>
               </div>
-              <Button
-                padding="py-2 px-4"
-                className="mt-8 w-auto"
-                category="white"
-                text="Generate QR"
-                type="submit"
-                loading={loading}
-              ></Button>
-            </div>
-          </form>
-        )}
-      </SecondScreen>
-    </section>
+              <div className="flex flex-col gap-1 w-full mt-3">
+                <label htmlFor="playstore" className="text-lg">
+                  Play Store
+                </label>
+                <div className="w-full flex items-center border border-zinc-800 rounded-md px-3 py-4 gap-2">
+                  <FaGooglePlay className="text-gray-500" size={24} />
+                  <input
+                    type="text"
+                    id="playstore"
+                    name="playstore"
+                    placeholder="Enter App Store URL"
+                    className=" bg-black focus:outline-none w-full"
+                    autoCapitalize="off"
+                    autoComplete="off"
+                  />
+                </div>
+                <Button
+                  padding="py-2 px-4"
+                  className="mt-8 w-auto"
+                  category="white"
+                  text="Generate QR"
+                  type="submit"
+                  loading={loading}
+                ></Button>
+              </div>
+            </form>
+          )}
+        </SecondScreen>
+      </section>
+    </AppLayout>
   );
 }
